95ad56cc412522ab3d2dcee7ef310437a3c81690
nyanmark <61084280+nyanmark@users.noreply.github.com>
2022-09-23 23:26:39 +0300

    Signed Commit

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import math
- 
+# Comment
 string = input("Degrees, Minutes, Seconds: ")
 array = string.split(":")
 degrees = int(array[0]) + (int(array[1])/60) + (int(array[2])/3600)
 radians = degrees * (math.pi/180)
- 
+# Comment 2
 print(radians)
