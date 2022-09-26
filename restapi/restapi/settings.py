ef668b094e364534f13a0ab7e3c9d83cf28e05ad
nyanmark <61084280+nyanmark@users.noreply.github.com>
2022-09-27 01:17:30 +0300

    Overhaul (Static File Server, MySQL Support)

--- restapi/restapi/settings.py
+++ restapi/restapi/settings.py
@@ -17,7 +17,7 @@ import logging.config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_ROOT = "../static-host/restapi"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_mysql',
     'corsheaders',
     'rest_framework',
     'restapi',
@@ -143,7 +144,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = os.getenv('DJANGO_STATIC_URL', 'http://127.0.0.1:8080/restapi/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
